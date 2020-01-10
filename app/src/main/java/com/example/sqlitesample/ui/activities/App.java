3c01f25f02470fd5634d6aca6099fabae51bf818
thawzinaung <thawzinaung@aceplussolutions.com>
2020-01-10 16:53:50 +0630

    Test push

---
+++ app/src/main/java/com/example/sqlitesample/ui/activities/App.java
@@ -0,0 +1,14 @@
+package com.example.sqlitesample.ui.activities;
+
+import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
+}
